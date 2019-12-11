78fe54ef76c9f8831bcfc4e331dd26df60236255
noidsoup <nicholas.eymann@gmail.com>
2019-12-10 19:16:05 -0800

    added bootstrap and a test drop down button

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,5 @@
-console.log('main.js is loaded!');
+
+$(document).ready(function(e) {
+  var bootstrap_enabled = (typeof $().modal == 'function');
+  console.log('main.js is loaded!', bootstrap_enabled);
+});
