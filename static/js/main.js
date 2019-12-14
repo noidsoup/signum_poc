2ee525b9af69f0732a25838e872f213d62b1d5d9
noidsoup <nicholas.eymann@gmail.com>
2019-12-13 17:58:24 -0800

    added category pill to insight page, author, and og tags to header

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,21 @@
-
-$(document).ready(function(e) {
-  var bootstrap_enabled = (typeof $().modal == 'function');
-  console.log('main.js is loaded!', bootstrap_enabled);
-});
+/***********************
+ * Smooth scroll on a new page
+ * http://stackoverflow.com/questions/9652944/jquery-scroll-to-id-from-different-page
+ ***********************/
+var jump=function(e)
+{
+	if (e){
+	   e.preventDefault();
+	   var target = jQuery(this).attr("href");
+	}else{
+	   var target = location.hash;
+	}
+	var sticky_height = 73;
+	jQuery('html,body').animate(
+	{
+	   scrollTop: jQuery(target).offset().top - sticky_height
+	},800,function()
+	{
+	   location.hash = target;
+	});
+}
