4047ca18e13d3f06bea4639c7913ee2b52117b7b
noidsoup <nicholas.eymann@gmail.com>
2019-12-22 18:32:28 -0800

    fixed faq links

--- static/js/main.js
+++ static/js/main.js
@@ -53,11 +53,20 @@ $( document ).ready( function () {
 	/***********************
 	 * Open a FAQ when you jump to it
 	 ***********************/
+	const checkHash = () => {
+		var hash = location.hash;
+		if ( hash ) {
+			$( hash ).find( '.faq-answer' ).collapse( 'show' );
+			$('html, body').animate({scrollTop: '-=40px'}, 800);
+		}
+	}
+
 	var hash = location.hash;
 	if ( hash ) {
-		$( hash ).find( '.faq-answer' ).collapse( 'show' );
-		$('html, body').animate({scrollTop: '-=40px'}, 800);
+		checkHash();
 	}
+
+	window.onhashchange = checkHash;
 
 	/***********************
 	 * Fix FAQ links in footer
@@ -66,7 +75,7 @@ $( document ).ready( function () {
 		e.preventDefault();
 		e.stopPropagation();
 		var href = $(this).attr('href');
-		console.log(href);
+
 	});
 
 
@@ -675,7 +684,7 @@ $( document ).ready( function () {
 										sortedArray.forEach((item, index) => {
 											createRow(item, index, chart.id);
 											const chartCanvas = document.getElementById( `chart-${chart.id}-${index}` );
-											//console.log(lampHistory);
+
 											createLampChart(chartCanvas, lampHistory, item.ticker);
 										})
 
@@ -712,7 +721,6 @@ $( document ).ready( function () {
 
 	/* Create the Liquidity Lamp stacked bar chart */
 	const createLampChart = (chart, lampHistory, ticker) => {
-		//console.log(lampHistory);
 		const liquidityLampDate =
 			moment(
 				lampHistory.data
@@ -797,7 +805,6 @@ $( document ).ready( function () {
 							fetch(promise.mediaLink).then(resolved => {
 								resolved.text().then(csv => {
 									const liquidityLampHistoryResponse = window.Papa.parse(csv, { skipEmptyLines: true, header: true });
-									console.log('seriously?',liquidityLampHistoryResponse);
 									fetchLiquidityLampRankings(day, lampCharts, liquidityLampHistoryResponse);
 								});
 							});
