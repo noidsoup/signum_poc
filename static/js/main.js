c9aec8c63cba22a1c60f89cab4154c771be61ff4
Melanie Cooper <melanie@cooperbold.com>
2019-12-23 10:47:51 -0700

    Fix LL chart collapse icon overflow on mobile

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,3 @@
-	/***********************
-	 * Smooth scroll on a new page
-	 * http://stackoverflow.com/questions/9652944/jquery-scroll-to-id-from-different-page
-	 ***********************/
-	/* var jump=function(e)
-	{
-		if (e){
-		   e.preventDefault();
-		   var target = jQuery(this).attr("href");
-		}else{
-		   var target = location.hash;
-		}
-		var sticky_height = 73;
-		jQuery('html,body').animate(
-		{
-		   scrollTop: jQuery(target).offset().top - sticky_height
-		},800,function()
-		{
-		   location.hash = target;
-		});
-	} */
-
-
-
 $( document ).ready( function () {
 
 	$( '.scrollTo' ).click( function ( e ) {
@@ -331,7 +307,7 @@ $( document ).ready( function () {
 			},
 		],
 	};
-	
+
 	/*
 	Create the Quote Vector bar chart
 	*/
@@ -586,7 +562,7 @@ $( document ).ready( function () {
 		const chartCollapseContentWrapper = createElement("div", ["chart-collapse-content", "collapse"], "", false, (`item-${id}-${index}`) );
 		const chartCollapseContentDiv = createElement("div");
 		const chartWrapper = createElement("div", ["chart-wrapper"], "", [
-			{ field: "style", value: "height:280px; width:900px" },
+			{ field: "style", value: "height:280px; width:100%" },
 		]);
 
 		const chartCanvas = createElement("canvas", [], "", [], `chart-${id}-${index}`);
